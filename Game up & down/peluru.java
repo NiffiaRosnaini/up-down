72064f26a2e1bf904331d01fa9c0dd363360bfc1
DESKTOP-5HL3VMU\ASUS <niffiarosnaini@gmail.com>
2018-05-11 11:41:55 +0900

    Keempat

--- Game up & down/peluru.java
+++ Game up & down/peluru.java
@@ -8,12 +8,45 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class peluru extends Actor
 {
+    boolean removed;
+    public peluru()
+    {
+        removed = false;
+    }
     /**
      * Act - do whatever the peluru wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if (foundPesawat()) {
+            kill();
+        }
+        if (removed) {
+            return;
+        }
+    move();
+    }
+    public boolean foundPeluru()
+    {
+        Actor peluru = getOneObjectAtOffset(0, 0, peluru.class);
+        return peluru != null;
+    }
+    public void kill()
+    {
+        Actor peluru = getOneObjectAtOffset(0, 0, peluru.class);
+        if(peluru != null) {
+            getWorld().removeObject(peluru);
+        }
+    }
+    public void move()
+    {
+        if(getY()<=0)
+        {
+            removed = true;
+            getWorld().removeObject(this);
+            return;
+        }
+        setLocation(getX(), getY()-10);
+    }
 }
