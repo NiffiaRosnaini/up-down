7e10ed0b7ab9a22d2f2f2fd839d78fe908b5533a
Eka Kartikasari <38681092+ekakartika@users.noreply.github.com>
2018-05-13 12:56:00 +0800

    peluru selesai

--- Game up & down/peluru.java
+++ Game up & down/peluru.java
@@ -8,56 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class peluru extends Actor
 {
-    boolean removed;
-    public peluru() 
+    /**
+     * Act - do whatever the peluru wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
     {
-        removed = false;
-    }     
-    public void act()
-    { if (foundMonstr()){
-            kill();
-       }
-      if (foundMonstr()){
-            killl();
-        }
-      if (removed) {
-            return;
-        }
-        
-        move();
-    }
-    public boolean foundMonstr()
-    {
-        Actor monstr = getOneObjectAtOffset(0, 0, monstr.class);
-        return monstr != null;
-    }
-    public boolean foundMonstrl()
-    {
-        Actor monstrl = getOneObjectAtOffset(0, 0, monstrl.class);
-        return monstrl != null;
-    }
-    public void killl(){
-        Actot monstrl = getOneObjectAtOffset(0, 0, monstrl.class);
-        if (monstrl != null) {
-            getWorld().removeObject(monstrl);
-        }
-    }
-    public void kill()
-    {
-        Actor monstr = getOneObjectAtOffset(0, 0, monstr.class);
-        getWorld().removeObeject(monstr);
-    }
-    public void move()
-    {
-        if(getY()<=0)
-        {
-            removed = true;
-            getWorld().removedObject(this);
-            return;
-        }
-        setLocation(getX(), getY()-10);
-    }
+        // Add your action code here.
+    }    
 }
-   
-
-   
