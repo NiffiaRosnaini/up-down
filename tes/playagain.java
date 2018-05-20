a379eb24c72393c9cf035acb34a64acda0088f19
AL_MUTTAQIN\Riza Al Muttaqin <ekakartika457@gmail.com>
2018-05-20 13:41:31 +0800

    playagain&exit(beumsellesai)

---
+++ tes/playagain.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class playagain here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class playagain extends Actor
+{
+    /**
+     * Act - do whatever the playagain wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+      playagain playagain = new playagain();
+    }    
+}
